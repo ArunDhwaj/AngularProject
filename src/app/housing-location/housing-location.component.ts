d136cc7fdc3e9c329cada5deb71cddf2f922b53a
ArunDhwaj <sbp.arun.01@gmail.com>
2023-11-03 19:23:22 +0530

    Housing component dynamic value added with *ngFor

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Housinglocation } from '../housinglocation';
+
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocationInput.photo" alt="Exterior photo of {{housingLocationInput.name}}">
+    <h2 class="listing-heading">{{ housingLocationInput.name }}</h2>
+    <p class="listing-location">{{ housingLocationInput.city}}, {{housingLocationInput.state }}</p>
+  </section>
+`,
+
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
+
+  @Input() housingLocationInput!: Housinglocation;
 
 }
